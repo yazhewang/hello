313f392970e4938eec8789a8befab74cf76b792d
yazhewang <98105429+yazhewang@users.noreply.github.com>
2022-08-29 13:50:34 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Yazhe Wang
+ * Date: 2022/08/26
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hello World!\n");
+  printf("Yazhe Wang\n");
+
+  return 0;
+}
